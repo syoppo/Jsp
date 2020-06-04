140f7b38612c8bbc81d3dce1854757212c2bac3b
syoppo <58536569+syoppo@users.noreply.github.com>
2020-06-05 02:17:10 +0900

    Create pj02.java

---
+++ week8/pj02.java
@@ -0,0 +1,40 @@
+package ch10;
+
+public class pj02 {
+	private String name;
+	private String title;
+	private String email;
+	private String textarear;
+	private String pwd;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTextarear() {
+		return textarear;
+	}
+	public void setTextarear(String textarear) {
+		this.textarear = textarear;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+}
