f82e203c8b0083aa903c1b44f518a6ba8145d381
syoppo <58536569+syoppo@users.noreply.github.com>
2020-06-05 02:17:03 +0900

    Create pj01.java

---
+++ week8/pj01.java
@@ -0,0 +1,40 @@
+package ch10;
+
+public class pj01 {
+	private String num;
+	private String name;
+	private String grade;
+	private String subject;
+	private String hobby[];
+	
+	public String getNum() {
+		return num;
+	}
+	public void setNum(String num) {
+		this.num = num;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String[] getHobby() {
+		return hobby;
+	}
+	public void setHobby(String[] hobby) {
+		this.hobby = hobby;
+	}
+}
